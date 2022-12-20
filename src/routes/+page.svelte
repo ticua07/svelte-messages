577106ea47e40a03604a15a3c952ff0cb68167b0
Ticua <ticua@ticua.ar>
2022-12-19 23:52:17 -0300

    added subscription and initial fetching of messages along with a basic ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
 
-	import { sp, messages } from './lib/stores';
+	import { sp, type Message } from './lib/stores';
 	let unsubscribe = sp.channel('*'); // I don't have supabase's types and i'm too lazy to generate them.
+	let messages: Message[] = [];
 	sp.from('messages').select('*');
 
 	onMount(async () => {
+		sp.from('messages')
+			.select('*')
+			.order('id', { ascending: false })
+			.then((data: NonNullable<{ data: Message[] | null }>) => {
+				messages = data.data || [];
+			});
+
 		unsubscribe
-			.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, (payload) =>
-				console.log(payload)
+			.on(
+				'postgres_changes',
+				{ event: 'INSERT', schema: 'public', table: 'messages' },
+				(payload: { new: Message }) => (messages = [payload.new, ...messages])
 			)
 			.subscribe();
 	});
@@ -20,3 +30,26 @@
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+
+{#each messages as message (message.id)}
+	<div class="message">
+		<h1>{message.title}</h1>
+		<p>{message.message}</p>
+		<small>{message.id}</small>
+	</div>
+{/each}
+
+<style>
+	.message {
+		border: 2px solid black;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 20px;
+	}
+
+	.message > * {
+		margin: 0;
+	}
+</style>
--- src/routes/lib/stores.ts
+++ src/routes/lib/stores.ts
@@ -1,17 +1,14 @@
-import { writable, type Writable } from 'svelte/store';
 import { createClient } from '@supabase/supabase-js';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 // Create a single supabase client for interacting with your database
-export const sp = createClient(process.env.SUPABASE_URL || '', process.env.SUPABASE_ANON || '');
+export const sp = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON
+);
 
-interface Message {
+export interface Message {
 	id: number;
 	title: string;
 	message: string;
 	created_at: Date;
 }
-
-export const messages: Writable<Message> = writable();
