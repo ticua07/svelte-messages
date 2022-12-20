3a39539d6af59d0d7f449b0dba7fa4f1998b5505
Ticua <ticua@ticua.ar>
2022-12-20 00:54:15 -0300

    reworked ui and added working submit form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,29 +27,126 @@
 		// unsubscribe on leave to avoid memory leaks
 		unsubscribe.unsubscribe();
 	});
+
+	const submitMessage = async (event: any) => {
+		const data = new FormData(event.target);
+
+		await sp.from('messages').insert({
+			title: data.get('title'),
+			message: data.get('message')
+		});
+	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-
-{#each messages as message (message.id)}
-	<div class="message">
-		<h1>{message.title}</h1>
-		<p>{message.message}</p>
-		<small>{message.id}</small>
-	</div>
-{/each}
+<svelte:head>
+	<style>
+		body {
+			background-color: #121212;
+		}
+	</style>
+</svelte:head>
+<div class="container">
+	<form class="form" on:submit|preventDefault={submitMessage}>
+		<input class="input" type="text" name="title" id="" placeholder="Title" />
+		<textarea
+			class="input"
+			type="text"
+			name="message"
+			id=""
+			rows="5"
+			placeholder="Message content"
+		/>
+		<button class="submit-btn">Submit message</button>
+	</form>
+	{#each messages as message (message.id)}
+		<div class="message">
+			<h1>{message.title}</h1>
+			<p>{message.message}</p>
+		</div>
+	{/each}
+</div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
+	.container {
+		padding: 50px;
+		padding-block: 15px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.container > * {
+		color: white;
+	}
+
+	.form {
+		display: flex;
+		flex-direction: column;
+		max-width: 550px;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		gap: 15px;
+		background-color: #242424;
+		padding: 20px;
+		border-radius: 5px;
+	}
+
+	.input {
+		background-color: #565656;
+		color: white;
+		border-radius: 5px;
+		padding: 5px;
+		font-size: 14px;
+		font-family: 'Noto Sans';
+		border: none;
+		outline: none;
+		resize: none;
+	}
+
+	.submit-btn {
+		background-color: #464646;
+		color: white;
+		border-radius: 5px;
+		padding: 8px;
+		font-size: 14px;
+		font-family: 'Noto Sans';
+		border: none;
+		outline: none;
+		resize: none;
+	}
+
+	.form > * {
+		width: 100%;
+		box-sizing: border-box;
+	}
 	.message {
-		border: 2px solid black;
+		border: 1px solid gray;
+		max-width: 550px;
+		width: 100%;
 		border-radius: 10px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		gap: 20px;
+		padding: 15px;
 	}
 
 	.message > * {
 		margin: 0;
+	}
+
+	.message > p {
+		font-family: 'Noto Sans';
+	}
+
+	.message > h1 {
+		font-family: 'Fira Sans';
+		font-style: normal;
+		font-weight: 500;
 	}
 </style>
--- src/routes/lib/stores.ts
+++ src/routes/lib/stores.ts
@@ -3,7 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 // Create a single supabase client for interacting with your database
 export const sp = createClient(
 	import.meta.env.VITE_SUPABASE_URL,
-	import.meta.env.VITE_SUPABASE_ANON
+	import.meta.env.VITE_SUPABASE_ANON,
+	{
+		global: {
+			fetch: fetch
+		}
+	}
 );
 
 export interface Message {
